43a44c70a7d69dac9806ddf6047fc61f4003c52e
amanbhandari04 <ab9650315@gmail.com>
2025-04-23 12:55:41 +0530

    create component folder

--- src/Component/CardForbellroy/ProductCard.jsx
+++ src/Component/CardForbellroy/ProductCard.jsx
@@ -1,30 +1,35 @@
 // ProductCard.js
 import React, { useState } from 'react';
-import bag from '../assets/bag-removebg-preview.png';
-import redBag from '../assets/red_bag-removebg-preview.png';
-import greenBag from '../assets/green_bag-removebg-preview.png';
+import bag from '../../assets/bag-removebg-preview.png';
+import redBag from '../../assets/red_bag-removebg-preview.png';
+import greenBag from '../../assets/green_bag-removebg-preview.png';
 import { CiShoppingCart } from "react-icons/ci";
 import "./CardForBellroy.css";
 
 const ProductCard = ({ defaultImage }) => {
+  // use state for color option
   const [hideColor, setHideColor] = useState(false);
   const [selectedColor, setSelectedColor] = useState("default");
 
+// hideColor is used to show and hide the color option
   const toggleColors = () => {
     setHideColor(!hideColor);
   };
 
+  // colors array for color option
   const colors = [
     { id: 1, name: 'red', hex: '#d30b04' },
     { id: 2, name: 'green', hex: '#008000' },
     { id: 3, name: 'brown', hex: '#411e0b' }
   ];
 
+  // handleColorSelect is used to set the selected color
   const handleColorSelect = (colorName) => {
     setSelectedColor(colorName);
     setHideColor(false);
   };
 
+// getImageByColor is used to get the image by color
   const getImageByColor = () => {
     switch (selectedColor) {
       case 'red':
@@ -42,6 +47,10 @@ const ProductCard = ({ defaultImage }) => {
     <div className="card">
       <div className="card-image">
         <img src={getImageByColor()} alt={`${selectedColor} bag`} />
+        <div className="card-image-overlay">
+          <h2 className="card-image-title">Bellroy</h2>
+         
+          </div>
       </div>
       <div className="card-content">
         <h3 className="card-title">NameOfYourProduct</h3>
--- src/MoreProduct.jsx/CardForProduct.jsx
+++ src/MoreProduct.jsx/CardForProduct.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import bag from '../assets/bag-removebg-preview.png';
 import redBag from '../assets/red_bag-removebg-preview.png';
+import greenBag from '../assets/green_bag-removebg-preview.png';
 import ProductCard from '../Component/CardForbellroy/ProductCard';
 
 const CardForProduct = () => {
   return (
     <main className="card-div">
       <ProductCard defaultImage={bag} />
-     
+      <ProductCard defaultImage={redBag} />
+      <ProductCard defaultImage={greenBag} />
     </main>
   );
 };
