f8421133b81e393713fb94dad1c12cfe3b47eaa2
amanbhandari04 <ab9650315@gmail.com>
2025-04-23 12:27:29 +0530

    create component folder

--- src/MoreProduct.jsx/CardForProduct.jsx
+++ src/MoreProduct.jsx/CardForProduct.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import bag from '../assets/bag-removebg-preview.png';
 import redBag from '../assets/red_bag-removebg-preview.png';
-import ProductCard from '../CardForbellroy/ProductCard';
+import ProductCard from '../Component/CardForbellroy/ProductCard';
 
 const CardForProduct = () => {
   return (
     <main className="card-div">
       <ProductCard defaultImage={bag} />
-      <ProductCard defaultImage={redBag} />
-      <ProductCard defaultImage={bag} />
+     
     </main>
   );
 };
